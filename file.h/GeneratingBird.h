7b4b895e85ed0964b831e41f158f83c464b5b760
Dainn98 <tuananhk56a1@gmail.com>
2024-03-06 23:15:06 +0700

    version 1.7.1.0

---
+++ file.h/GeneratingBird.h
@@ -0,0 +1,32 @@
+#ifndef GENERATING_BIRD_H
+#define GENERATING_BIRD_H
+#include "CommonFunc.h"
+class Bird {
+public:
+    int x, y, velocity, currentFrame,width,height;
+    Bird() {
+        x = SCREEN_WIDTH / 4;
+        y = SCREEN_HEIGHT / 2;
+        velocity = 0;
+        currentFrame = 0;
+        width = BIRD_WIDTH;
+        height = BIRD_HEIGHT;
+    }
+    void jump() {
+        velocity = BIRD_JUMP_VELOCITY;
+    }
+    void update() {
+        velocity += GRAVITY;
+        y += velocity;
+        currentFrame = (currentFrame + 1) % FLYING_ANIMATION_FRAMES_OF_BIRD;
+    }
+    void render() {
+        gTextures[currentFrame].render(x, y);
+    }
+    SDL_Rect strikeObstacle() const {
+        SDL_Rect Obstacle = { x, y, width, height };
+        return Obstacle;
+    }
+};
+
+#endif
